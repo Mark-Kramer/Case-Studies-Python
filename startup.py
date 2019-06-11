577940d44a1a7908647af57ea38ae66a5b01dbb6
Emily <eds2@bu.edu>
2019-06-10 23:33:29 -0400

    added startup file to make default imports easier

---
+++ startup.py
@@ -0,0 +1,13 @@
+from scipy.io import loadmat
+import warnings
+from matplotlib import rcParams
+from matplotlib.pyplot import plot, subplots, xlim, xlabel, ylim, ylabel, show, title, legend, style, semilogx, semilogy, savefig
+import numpy as np
+from numpy import zeros_like, ones_like, arange
+style.use('ggplot')
+rcParams['backend'] = 'TkAgg'
+subplots()
+rcParams['figure.figsize'] = (12, 3)
+warnings.simplefilter(action='ignore', category=FutureWarning)
+def figsize(w=12, h=3):
+    rcParams['figure.figsize'] = (w, h)
