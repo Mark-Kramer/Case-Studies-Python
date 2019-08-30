6561d562902f3e875343a6663d9a29da1f975e41
Emily <eds2@bu.edu>
2019-08-30 18:34:02 -0400

    Updating ch10

--- startup/00_imports.py
+++ startup/00_imports.py
@@ -2,8 +2,8 @@ from scipy.io import loadmat  # To load .mat files
 import matplotlib.pyplot as plt  # Load plotting functions
 # from matplotlib import rcParams  # Allow us to change appearance of figures
 # Import specific plotting functions that we use frequently
-from numpy import *  # Import numerical functions
 from pylab import *  # Import plotting functions
+from numpy import *  # Import numerical functions
 from IPython.core.pylabtools import figsize  # Allow us to change figure sizes
 # Package for manipulating appearance of notebooks
 from IPython.core.display import HTML
