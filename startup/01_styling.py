d2933754728bd1733e7165d96e8729fb72c7b727
Emily <eds2@bu.edu>
2020-04-28 14:15:31 -0400

    rebuild

--- startup/01_styling.py
+++
@@ -1,3 +0,0 @@
-# HTML('../../assets/custom/custom.css')  # Change some style features of notebooks
-style.use(['ggplot', '~/.jupyter/custom/plotstyle.mplstyle'])  # Change default plot appearance
-get_ipython().run_line_magic('matplotlib', 'inline')
