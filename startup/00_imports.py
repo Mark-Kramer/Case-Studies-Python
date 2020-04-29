cf57832dbe89fe98796dc92411672a084cbe4e44
Emily <eds2@bu.edu>
2020-04-29 17:44:15 -0400

    rebuild

--- startup/00_imports.py
+++ startup/00_imports.py
@@ -1,12 +1,8 @@
-from scipy.io import loadmat  # To load .mat files
-import matplotlib.pyplot as plt  # Load plotting functions
-# from matplotlib import rcParams  # Allow us to change appearance of figures
-# Import specific plotting functions that we use frequently
-from pylab import *  # Import plotting functions
-from numpy import *  # Import numerical functions
-from IPython.core.pylabtools import figsize  # Allow us to change figure sizes
-# Package for manipulating appearance of notebooks
-from IPython.core.display import HTML
-# Package for displaying YouTube videos
-from IPython.lib.display import YouTubeVideo
-
+# Import the usual suspects ...
+from scipy.io import loadmat                    # To load .mat files
+import matplotlib.pyplot as plt                 # Load plotting functions
+from pylab import *                             # Import plotting functions
+from numpy import *                             # Import numerical functions
+from IPython.core.pylabtools import figsize     # Allow us to change figure sizes
+from IPython.core.display import HTML           # Package for manipulating appearance of notebooks
+from IPython.lib.display import YouTubeVideo    # Package for displaying YouTube videos
