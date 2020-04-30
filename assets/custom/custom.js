2a7c31a198dc915a74d0161abee26429f30ff01e
Emily <eds2@bu.edu>
2020-04-30 12:57:55 -0400

    create js to translate md in questions

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,1 +1,14 @@
 // Put your custom javascript here
+// <script type="text/javascript" src="https://rawgit.com/showdownjs/showdown/develop/dist/showdown.min.js"></script>
+
+function load_script() {
+	var converter = new showdown.Converter();
+
+	ii = 0;
+	X = document.getElementsByClassName("question");
+	while (X[ii]) {
+		X[ii].innerHTML = converter.makeHtml(X[ii].innerText);
+		// X[ii].innerHTML = "Hello";
+		ii++;
+	}
+}
