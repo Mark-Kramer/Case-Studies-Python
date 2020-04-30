8f06fe959c1d3820e73825556447bfb9723c976d
Emily <eds2@bu.edu>
2020-04-30 14:11:16 -0400

    minor cleaning

--- assets/custom/custom.js
+++ assets/custom/custom.js
@@ -1,5 +1,4 @@
 // Put your custom javascript here
-// <script type="text/javascript" src="https://rawgit.com/showdownjs/showdown/develop/dist/showdown.min.js"></script>
 
 
 var script = document.createElement('script');
@@ -9,17 +8,16 @@ script.type = "text/javascript";
 
 document.head.appendChild(script);
 
-function load_script() {
+function md2html() {
 	var converter = new showdown.Converter();
 	var classes = ["question", "python-note", "warning", "math-note"];
-	c = 0;
 
+	c = 0;
 	while (classes[c]) {
 		ii = 0;
 		X = document.getElementsByClassName(classes[c]);
 		while (X[ii]) {
 			X[ii].innerHTML = converter.makeHtml(X[ii].innerHTML);
-			// X[ii].innerHTML = "Hello";
 			ii++;
 		}
 		c++;
@@ -27,6 +25,5 @@ function load_script() {
 }
 
 document.addEventListener('turbolinks:load', () => {
-  load_script()
+  md2html()
 })
-// initFunction(load_script);
