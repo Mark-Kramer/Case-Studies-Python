a6995e85acedbf7e58a308853da5193b43cfe839
Emily <eds2@bu.edu>
2020-06-12 15:05:40 -0400

    back to start

--- _site/startup/00_imports.py
+++
@@ -1,8 +0,0 @@
-# Import the usual suspects ...
-from scipy.io import loadmat                    # To load .mat files
-import matplotlib.pyplot as plt                 # Load plotting functions
-from pylab import *                             # Import plotting functions
-from numpy import *                             # Import numerical functions
-from IPython.core.pylabtools import figsize     # Allow us to change figure sizes
-from IPython.core.display import HTML           # Package for manipulating appearance of notebooks
-from IPython.lib.display import YouTubeVideo    # Package for displaying YouTube videos
--- _site/startup/01_styling.py
+++
@@ -1,4 +0,0 @@
-# HTML('../../assets/custom/custom.css')  # Change some style features of notebooks
-# style.use(['ggplot', '~/.jupyter/custom/plotstyle.mplstyle'])  # Change default plot appearance
-style.use(['ggplot'])  # Change default plot appearance
-get_ipython().run_line_magic('matplotlib', 'inline')
