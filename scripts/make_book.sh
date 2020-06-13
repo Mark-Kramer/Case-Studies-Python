c3d277da99dd8e095774859679c9c879ce1872eb
Emily <eds2@bu.edu>
2020-06-13 13:46:42 -0400

    update config and csn.yml

--- scripts/make_book.sh
+++ scripts/make_book.sh
@@ -2,9 +2,9 @@
 
 cp -r _config/* _book/
 cp -r content/* _book/
-ls content/*ipynb > file_list.txt
-while IFS=["/","."] read -r content file ext; do rm _book/$file.md ; done < file_list.txt
+ls content/*ipynb | while IFS=["/","."] read -r content file ext; do rm _book/$file.md ; done 
 jb build _book
-rm file_list.txt
+tail -n +2 _book/_toc.yml > _book/_toc1.yml
+jb build --toc _book/_toc1.yml _book 
 cp -r content/imgs _book/_build/html/
 
