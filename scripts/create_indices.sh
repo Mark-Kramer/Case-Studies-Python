8aa9935e1546a3c2850b2e1732766bcf1b57e35b
Emily <eds2@bu.edu>
2020-06-21 18:06:10 -0400

    Update documentation

--- scripts/create_indices.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ls content/*/*.ipynb > file_list.txt
-
-while IFS=["/","."] read -r build ch file ext; do echo "<meta http-equiv=\"Refresh\" content=\"0; url=$file.html\" />" > $build/$ch/index.html; done < file_list.txt
-
-rm file_list.txt
-
