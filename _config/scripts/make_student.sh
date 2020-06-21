6537ed1ca310018cb1a44eae901f78281b48c8ff
Emily <eds2@bu.edu>
2020-06-21 18:40:32 -0400

    cleaning

--- _config/scripts/make_student.sh
+++ _config/scripts/make_student.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 mkdir tmp
-cp -r student/* tmp
+if test -d student ; then cp -r student/* tmp ; fi
 while IFS=[" "] read -r a b fname ; do 
     if [ "$fname" ] && test -f "$fname" ; then 
         cp $fname tmp/ ; 
     fi; 
-done < _toc.yml
+done < _config/_toc.yml
 
 ln -s $PWD/{matfiles,imgs} tmp/
 cp *.py tmp
@@ -14,7 +14,7 @@ cp README.md tmp
 mkdir tmp/config
 cp environment.yml tmp/config
 cp -r _config/startup tmp/config/
-cp _static/custom.* tmp/config/
+cp _config/_static/custom.* tmp/config/
 cp _config/Makefile.student tmp/Makefile
 
 ghp-import -p -f -l -b student tmp/
