b7a49c42686a2a74dbbd8e7213e618cc88053828
Emily <eds2@bu.edu>
2020-06-22 17:04:39 -0400

    Update documentation

---
+++ _config/startup/01_styling.py
@@ -0,0 +1,6 @@
+# HTML('../../assets/custom/custom.css')  # Change some style features of notebooks
+# style.use(['ggplot', '~/.jupyter/custom/plotstyle.mplstyle'])  # Change default plot appearance
+style.use(['ggplot'])  # Change default plot appearance
+get_ipython().run_line_magic('matplotlib', 'inline')
+figsize(12, 3)
+
